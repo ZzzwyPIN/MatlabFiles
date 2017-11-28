87088ec66be74a8fdc2b854697b39e6c32fd1f64
ZzzwyPIN <ZzzwyPIN@foxmail.com>
2017-11-28 22:33:55 +0800

    Test git

--- CDMA_System/DssCommunication.m
+++ CDMA_System/DssCommunication.m
@@ -20,15 +20,6 @@ N=2^length(pntaps1)-1;
 M=10000;
 B=2*randi([0,1],K,M)-ones(K,M);
 
-% for k=1:M
-%     for i=1:N
-%         A1=0;
-%         for j=1:K
-%             A1=A1+B(j,k).*C(j,i);
-%         end;
-%         A(i,k)=A1;
-%     end;
-% end;
 A=zeros(1,N*M);
  for i=1:K
         for j=1:M
@@ -51,23 +42,6 @@ axis auto;
 title('信噪比一定时,用户数和误码率的关系');
 xlabel('用户数K');
 ylabel('误比特率BER');
-
-
-% R0=C*C';
-
-% for i=1:M
-%  X(:,i)= schur(R0,B(:,i));
-% end
-% errors=CountError(X,B,K,M);
-% users=1:K;
-% semilogy(users,errors/(K*M),'r-*');
-
-% for i=1:M
-% X(:,i)=(inv(R0))*B(:,i);%解相关，就是求R逆
-% end
-% errors=counterror(X,B,K,M);
-% users=1:K;
-% semilogy(users,errors/(K*M),'b-^');
 
 
             
