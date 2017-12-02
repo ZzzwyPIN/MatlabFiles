fa9e08cf5dd8fc803a82790a500f0d784de22447
Zzzwy <ZzzwyPIN@foxmail.com>
2017-12-02 22:05:36 +0800

    看完第五章源代码

--- Contemporary_Conmmunication_System(Using MATLAB and Simulink)/Chapter5/Smldp510.m
+++ Contemporary_Conmmunication_System(Using MATLAB and Simulink)/Chapter5/Smldp510.m
@@ -1,22 +1,22 @@
-function [p]=smldP510(snr_in_dB)
+function [p]=Smldp510(snr_in_dB)
 % [p]=smldP510(snr_in_dB)
 %		SMLDP510  simulates the probability of error for the given
 %   		snr_in_dB, signal-to-noise ratio in dB.
 M=4;				   	% quaternary orthogonal signaling
 E=1;
 SNR=exp(snr_in_dB*log(10)/10);	   	% signal-to-noise ratio per bit
-sgma=sqrt(E^2/(4*SNR));  	   	% sigma, standard deviation of noise
+sgma=sqrt(E^2/(4*SNR));  	   	% sigma, standard deviation of noise 关键是不知道为什么sgma可以这样计算得出。
 N=10000;	 		   	% number of symbols being simulated
 % generation of the quaternary data source
-for i=1:N,
+for i=1:N
   temp=rand;   	      			% a uniform random variable over (0,1)
-  if (temp<0.25),
+  if (temp<0.25)
     dsource1(i)=0; 
     dsource2(i)=0;	      	
-  elseif (temp<0.5),
+  elseif (temp<0.5)
     dsource1(i)=0; 
     dsource2(i)=1;	      	
-  elseif (temp<0.75),
+  elseif (temp<0.75)
     dsource1(i)=1; 
     dsource2(i)=0;	      	
   else
@@ -26,38 +26,38 @@ for i=1:N,
 end;
 % detection, and probability of error calculation
 numoferr=0;
-for i=1:N,
+for i=1:N
   % matched filter outputs
-  if ((dsource1(i)==0) & (dsource2(i)==0)),
-    r0=sqrt(E)+gngauss(sgma);
-    r1=gngauss(sgma);
-    r2=gngauss(sgma);
-    r3=gngauss(sgma);
-  elseif ((dsource1(i)==0) & (dsource2(i)==1)),
-    r0=gngauss(sgma);
-    r1=sqrt(E)+gngauss(sgma);
-    r2=gngauss(sgma);
-    r3=gngauss(sgma);
-  elseif ((dsource1(i)==1) & (dsource2(i)==0)),
-    r0=gngauss(sgma);
-    r1=gngauss(sgma);
-    r2=sqrt(E)+gngauss(sgma);
-    r3=gngauss(sgma);
+  if ((dsource1(i)==0) && (dsource2(i)==0))
+    r0=sqrt(E)+Gngauss(sgma);
+    r1=Gngauss(sgma);
+    r2=Gngauss(sgma);
+    r3=Gngauss(sgma);
+  elseif ((dsource1(i)==0) && (dsource2(i)==1))
+    r0=Gngauss(sgma);
+    r1=sqrt(E)+Gngauss(sgma);
+    r2=Gngauss(sgma);
+    r3=Gngauss(sgma);
+  elseif ((dsource1(i)==1) && (dsource2(i)==0))
+    r0=Gngauss(sgma);
+    r1=Gngauss(sgma);
+    r2=sqrt(E)+Gngauss(sgma);
+    r3=Gngauss(sgma);
   else
-    r0=gngauss(sgma);
-    r1=gngauss(sgma);
-    r2=gngauss(sgma);
-    r3=sqrt(E)+gngauss(sgma);
+    r0=Gngauss(sgma);
+    r1=Gngauss(sgma);
+    r2=Gngauss(sgma);
+    r3=sqrt(E)+Gngauss(sgma);
   end;
   % the detector
   max_r=max([r0 r1 r2 r3]);
-  if (r0==max_r),
+  if (r0==max_r)
     decis1=0;
     decis2=0;
-  elseif (r1==max_r),
+  elseif (r1==max_r)
     decis1=0;
     decis2=1;
-  elseif (r2==max_r),
+  elseif (r2==max_r)
     decis1=1;
     decis2=0;
   else
@@ -65,10 +65,10 @@ for i=1:N,
     decis2=1;
   end;
   % Count the number of bit errors made in this decision.
-  if (decis1~=dsource1(i)),   		% If it is an error, increase the error counter.
+  if (decis1~=dsource1(i))   		% If it is an error, increase the error counter.
     numoferr=numoferr+1;
   end;
-  if (decis2~=dsource2(i)),   		% If it is an error, increase the error counter.
+  if (decis2~=dsource2(i))   		% If it is an error, increase the error counter.
     numoferr=numoferr+1;
   end;
 end;
--- Contemporary_Conmmunication_System(Using MATLAB and Simulink)/Chapter5/ip_05_09.m
+++ Contemporary_Conmmunication_System(Using MATLAB and Simulink)/Chapter5/ip_05_09.m
@@ -3,13 +3,13 @@ echo on
 SNRindB1=5:1:25;	  		
 SNRindB2=5:0.1:25;			
 M=16;
-for i=1:length(SNRindB1),
+for i=1:length(SNRindB1)
   % simulated error rate
-  smld_err_prb(i)=smldPe59(SNRindB1(i));    
+  smld_err_prb(i)=Smldpe59(SNRindB1(i));
   echo off;
 end;
 echo on ;
-for i=1:length(SNRindB2),
+for i=1:length(SNRindB2)
   SNR_per_bit=exp(SNRindB2(i)*log(10)/10);  
   % theoretical error rate
   theo_err_prb(i)=(2*(M-1)/M)*Qfunct(sqrt((6*log2(M)/(M^2-1))*SNR_per_bit));
